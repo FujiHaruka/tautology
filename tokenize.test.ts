432f0b14b0f8ff167ef8564d07cb9a924c60a045
FujiHaruka <fizzyjohn1427@gmail.com>
2022-09-23 09:57:19 +0900

    Create deps-test.ts

--- tokenize.test.ts
+++ tokenize.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.156.0/testing/asserts.ts";
+import { assertEquals } from "./deps-test.ts";
 import { token, tokenize } from "./tokenize.ts";
 
 Deno.test("tokenize", () => {
